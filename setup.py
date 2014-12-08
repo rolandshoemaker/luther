acf14ca7cce24df6ef11ef082aef03bae3765644
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-12-08 03:59:35 -0800

    correct flask

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
         'flask-httpauth',
+        'Flask',
         'dnspython3',
         'click',
         'tabulate'
